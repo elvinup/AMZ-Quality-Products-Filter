9da202276a8e54c1202f2fc8d174891042658b87
Elvin Uthuppan <elvinutheman@gmail.com>
2016-04-03 21:24:48 -0400

    Actually Removes Products!
    Filters out product based on the keyword variable

--- js/content.js
+++ js/content.js
@@ -1,36 +1,13 @@
-//content.js
+window.onload = function() {
+  var titles = $(".title").toArray();
+  var boxes = $(".box").toArray();
+  var length = 127;
+  for (i = 0; i < length; i++) {
+      var str = titles[i].innerHTML.toLowerCase();
+      var keyword = str.indexOf("women's");
+      if (keyword > 0) {
+          $(boxes[i]).remove();
+      }
+  }
+};
 
-//alert("Hello from your chrome extension!");
-
-//function removeJunk() {
-	var parent = document.getElementbyId("box-container-inner");
-	var title = document.getElementbyId("box");
-	parent.removeChild(title);
-//}
-
-//function removeJunk() {
-   // var list = document.getElementById('box-container');
-   // var node = list.item(0);
-  // list.remove();
-   
-  
-  //$('#box-container-inner .title').remove();
- /*   return false;
-}
-function pageInit() {
-    // Hook up the "remove dummy" button
-    var btn = document.getElementById('btnRemoveJunk');
-    if (btn.addEventListener) {
-        // DOM2 standard
-        btn.addEventListener('click', removeJunk, false);
-    }
-    else if (btn.attachEvent) {
-        // IE (IE9 finally supports the above, though)
-        btn.attachEvent('onclick', removeJunk);
-    }
-    else {
-        // Really old or non-standard browser, try DOM0
-        btn.onclick = removeJunk;
-    }
-}
-*/
